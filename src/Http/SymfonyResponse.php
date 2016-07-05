0fcc770d7eba2127b560fca711d81e9b08cc371d
Hannes Van De Vreken <vandevreken.hannes@gmail.com>
2016-07-05 16:25:07 +0200

    StreamInterfaces worked cause they have __toString, but now it's actually streamed

--- src/Http/SymfonyResponse.php
+++ src/Http/SymfonyResponse.php
@@ -1,8 +1,10 @@
 <?php
 namespace Madewithlove\Export\Http;
 
+use Psr\Http\Message\StreamInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\ResponseHeaderBag;
+use Symfony\Component\HttpFoundation\StreamedResponse;
 
 trait SymfonyResponse
 {
@@ -14,7 +16,20 @@ trait SymfonyResponse
      */
     private function fileDownload($content, $filename)
     {
-        $response = new Response($content);
+        if ($content instanceof StreamInterface) {
+            $response = new StreamedResponse();
+
+            $response->setCallback(function () use ($content) {
+                $content->rewind();
+
+                while (! $content->eof()) {
+                    echo $content->read(8192);
+                }
+            });
+        } else {
+            $response = new Response($content);
+        }
+
         $disposition = $response->headers->makeDisposition(ResponseHeaderBag::DISPOSITION_ATTACHMENT, $filename);
         $response->headers->set('Content-Disposition', $disposition);
 
